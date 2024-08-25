760d91d88d479052e59e7652c5769486685743a5
Harshkaushik04 <142307211+Harshkaushik04@users.noreply.github.com>
2024-08-25 21:15:58 +0530

    Add files via upload

--- mapping.cpp
+++ mapping.cpp
@@ -8,44 +8,44 @@
 using namespace std;
 
 
-//page table implementation
+// //page table implementation
 
-class page{
-public:
-    int page_size;
-    unsigned int page_address;
-    unsigned int inner_pagetable_address;
-public:
-    page(int page_size,unsigned int page_address,unsigned int inner_pagetable_address){
-        this->page_size=page_size;
-        this->page_address=page_address;
-        this->inner_pagetable_address=inner_pagetable_address;
-    }
-};
-//second level page table
-class inner_page_table{ 
-public:
-    int inner_page_table_size;
-    unsigned int inner_pagetable_address;
-    unsigned int outer_pagertable_address;
-public:
-    inner_page_table(int inner_page_table_size,unsigned int inner_pagetable_address,unsigned int outer_pagertable_address){
-        this->inner_page_table_size=inner_page_table_size;
-        this->inner_pagetable_address=inner_pagetable_address;
-        this->outer_pagertable_address=outer_pagertable_address;
-    }
-};
-//first level page table
-class outer_page_table{
-public:
-    int outer_page_table_size;
-    unsigned int outer_page_table_address;
-public:
-    outer_page_table(int outer_page_table_size,unsigned int outer_page_table_address){
-        this->outer_page_table_size=outer_page_table_size;
-        this->outer_page_table_address=outer_page_table_address;
-    }
-};
+// class page{
+// public:
+//     int page_size;
+//     unsigned int page_address;
+//     unsigned int inner_pagetable_address;
+// public:
+//     page(int page_size,unsigned int page_address,unsigned int inner_pagetable_address){
+//         this->page_size=page_size;
+//         this->page_address=page_address;
+//         this->inner_pagetable_address=inner_pagetable_address;
+//     }
+// };
+// //second level page table
+// class inner_page_table{ 
+// public:
+//     int inner_page_table_size;
+//     unsigned int inner_pagetable_address;
+//     unsigned int outer_pagertable_address;
+// public:
+//     inner_page_table(int inner_page_table_size,unsigned int inner_pagetable_address,unsigned int outer_pagertable_address){
+//         this->inner_page_table_size=inner_page_table_size;
+//         this->inner_pagetable_address=inner_pagetable_address;
+//         this->outer_pagertable_address=outer_pagertable_address;
+//     }
+// };
+// //first level page table
+// class outer_page_table{
+// public:
+//     int outer_page_table_size;
+//     unsigned int outer_page_table_address;
+// public:
+//     outer_page_table(int outer_page_table_size,unsigned int outer_page_table_address){
+//         this->outer_page_table_size=outer_page_table_size;
+//         this->outer_page_table_address=outer_page_table_address;
+//     }
+// };
 
 bool string_keys_checker(unordered_map<string,vector<unsigned long long>> map,string to_check){
     bool flag=false;
